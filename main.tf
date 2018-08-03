a51673742e23bc265ba0518178d2328ffc490513
Jeremy Odle <jeremy.odle@gravicore.io>
2018-08-02 22:37:21 -0600

    Added password policy in support of CIS Amazon web Services Foundations v1.2.0

--- main.tf
+++ main.tf
@@ -5,6 +5,12 @@ terraform {
 module "gravicore_access" {
   source = "./modules/gravicore-access"
 
-  allow_gravicore_access    = true
+  allow_gravicore_access    = "${var.allow_gravicore_access}"
   trusted_entity_account_id = "${var.trusted_entity_account_id}"
+}
+
+module "iam" {
+  source = "./modules/iam"
+
+  tags = "${var.tags}"
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
+variable "tags" {
+  description = "https://aws.amazon.com/answers/account-management/aws-tagging-strategies/"
+  default     = {}
+}
 variable allow_gravicore_access {
   description = "Flag to establish SAML connectivity for Gravicore managed services"
   default     = false
@@ -9,4 +13,8 @@ variable trusted_entity_account_id {
 
 variable "iam_account_alias" {
   description = "The account alias to create."
+}
+
+variable "aws_region" {
+  default = "us-east-1"
 }
